7587f2823809ca6d4e71b4cc7c746d1b9f17d243
Michael Robinson <michael.robinson@kiwirail.co.nz>
2015-05-07 09:31:30 +1200

    Added mongo-mock

--- test/mongo.spec.js
+++ test/mongo.spec.js
@@ -9,21 +9,22 @@ var expect = require('chai').expect; //change0
  * First version - creating a mongodb simple API using callbacks
  */
 describe('mongodb api testing', function() {
-  var client = require('mongodb').MongoClient,
-      url = 'mongodb://localhost:27017/jdtdd',
-      connect; // change1
-  beforeEach(function(done){ // #exp1
+  var client = require('mongo-mock').MongoClient,
+    url = 'mongodb://localhost:27017/jdtdd',
+    connect; // change1
+  beforeEach(function(done) { // # exp1
     connect = client.connect(url, function(err, db) {
-      db.should.be.defined; //#exp2
-      expect(err).to.be.null; //#exp3
+      db.should.be.defined; // #exp2
+      expect(err).to.be.null; // #exp3
       db.close();
       done(); // #exp1
     });
   }); // change2
-  it('should have the client instantiated', function(){
+  it('should have the client instantiated', function() {
     client.should.be.defined;
   });
-  it('should insert a document to mongodb', function(done){
+  // it('should insert a document to mongodb', function(done) {
 
-  });
+  // });
 });
+
